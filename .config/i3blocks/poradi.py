8b602c2a818b147e4cccd2cc90c46c65fde63ec3
Prokop Randáček <prokop@randacek.dev>
2020-07-20 21:18:35 +0200

    i3!

---
+++ .config/i3blocks/poradi.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+import requests
+
+url = "http://i.protab.cz/poradi"
+
+r = requests.get(url)
+page = str(r.content).replace("\\n", "\n").replace("\\t", "")
+page = page[: page.index("Prokop")]
+poradi = page.split("\n")[-2][18:20]
+print(f"Jsem {poradi}.")
