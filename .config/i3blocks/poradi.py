6f6d62310c22a2b68ff3dd1ae3597b75d60a0558
Prokop Randáček <prokop@randacek.dev>
2020-07-25 20:07:04 +0200

    colors

--- .config/i3blocks/poradi.py
+++ .config/i3blocks/poradi.py
@@ -7,5 +7,5 @@ url = "http://i.protab.cz/poradi"
 r = requests.get(url)
 page = str(r.content).replace("\\n", "\n").replace("\\t", "")
 page = page[: page.index("Prokop")]
-poradi = page.split("\n")[-2][18:20]
+poradi = page.split("\n")[-2][18:20].replace(".", "")
 print(f"Jsem {poradi}.")
